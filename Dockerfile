9838c798870c0e4d1773392f928c78e2f3cd8bdc
yujin <northwindapps@gmail.com>
2025-06-04 17:41:58 +0900

    dockerization

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,6 @@ RUN pip3 install -r requirements.txt
 
 COPY . .
 
-CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
+EXPOSE 8000
+
+CMD [ "python3", "app.py"]
--- app.py
+++ app.py
@@ -48,8 +48,7 @@ def get_users():
     return jsonify(users)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
 
 
 
